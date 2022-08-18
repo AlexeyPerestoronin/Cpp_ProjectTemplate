0c737f150e0bda0f3e613638f82c4d6f0be5d0db
Alexey Perestoronin <Alexey.Perestoronin@yandex.ru>
2022-08-18 23:59:48 +0600

    add Boost as auto-downloaded external dependencies

--- AddGoogleTests.cmake
+++ AddGoogleTests.cmake
@@ -2,18 +2,22 @@ cmake_minimum_required(VERSION 3.14)
 
 include(FetchContent)
 
-# brief: add Google tests to current test-project
-# note1: the google tests project downloads from its git-repository and builds before building the test-project
-# note2: if google tests has been download and build early it not be do again
+# brief: add Google tests to current project
 function(AddGoogleTests)
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
         GIT_TAG release-1.12.1
+        GIT_PROGRESS true
     )
 
     # For Windows: Prevent overriding the parent project's compiler/linker settings
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
     FetchContent_MakeAvailable(googletest)
+
+    set_target_properties(gmock PROPERTIES FOLDER "External/GTests")
+    set_target_properties(gmock_main PROPERTIES FOLDER "External/GTests")
+    set_target_properties(gtest PROPERTIES FOLDER "External/GTests")
+    set_target_properties(gtest_main PROPERTIES FOLDER "External/GTests")
 endfunction(AddGoogleTests)
--- tests/unit-tests/test-unit-template-1.cpp
+++ tests/unit-tests/test-unit-template-1.cpp
@@ -2,7 +2,18 @@
 
 #include "src/default.hpp"
 
+#include <boost/coroutine/coroutine.hpp>
 
 TEST(SomeTest1, TestTest) {
-    ASSERT_EQ(twice(5), 10);
+    using ac_int = boost::coroutines::asymmetric_coroutine<int>;
+
+    ac_int::pull_type source([&](ac_int::push_type& sink) {
+        sink(10);
+        sink(20);
+        sink(30);
+    });
+
+    ASSERT_EQ(source.get(), 10);
+    ASSERT_EQ(source().get(), 20);
+    ASSERT_EQ(source().get(), 30);
 }
